1f9975650f20754cbaf29091ffa4dfcbfacedb00
seunghwanLee <dkqlch@naver.com>
2022-11-13 21:11:17 +0900

    User entity 수정

--- src/main/java/comu/community/entity/user/User.java
+++ src/main/java/comu/community/entity/user/User.java
@@ -22,6 +22,8 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false, unique = true)
     private String username;
 
+    private String sex;
+
     @JsonIgnore
     @Column(nullable = false)
     private String password;
