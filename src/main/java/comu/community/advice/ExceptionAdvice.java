c75bd7396ebd9a2eb6521413a53f3c3deda3ae87
seunghwanLee <dkqlch@naver.com>
2022-11-21 14:13:49 +0900

    상황별 exception 추가

--- src/main/java/comu/community/advice/ExceptionAdvice.java
+++ src/main/java/comu/community/advice/ExceptionAdvice.java
@@ -1,5 +1,9 @@
 package comu.community.advice;
 
+import comu.community.exception.CannotConvertHelperException;
+import comu.community.exception.LoginFailureException;
+import comu.community.exception.MemberNicknameAlreadyExistsException;
+import comu.community.exception.MemberUsernameAlreadyExistsException;
 import comu.community.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -8,7 +12,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.management.relation.RoleNotFoundException;
 
 @RestControllerAdvice
 @Slf4j
@@ -16,14 +19,23 @@ public class ExceptionAdvice {
 
     // 500 에러
     @ExceptionHandler(IllegalArgumentException.class) // 지정한 예외가 발생하면 해당 메소드 실행
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 각 예외마다 사애 코드 지정
     public Response illegalArgumentExceptionAdvice(IllegalArgumentException e) {
         log.info("e = {}", e.getMessage());
         return Response.failure(500, e.getMessage().toString());
     }
 
+    // 500 에러
+    // 컨버트 실패
+    @ExceptionHandler(CannotConvertHelperException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Response cannotConvertNestedStructureException(CannotConvertHelperException e) {
+        log.error("e = {}", e.getMessage());
+        return Response.failure(500, e.getMessage().toString());
+    }
+
     // 400 에러
-    // 요청 객체의 validation을 수행할 때, MethodArgumentNotValidException이 발생
+    // 요청 객체의 validation 을 수행할 때, MethodArgumentNotValidException 이 발생
     // 각 검증 어노테이션 별로 지정해놨던 메시지를 응답
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -34,31 +46,41 @@ public class ExceptionAdvice {
 
     // 401 응답
     // 아이디 혹은 비밀번호 오류시
-//    @ExceptionHandler(LoginFailureException.class)
-//    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-//    public Response loginFailureException() {
-//        return Response.failure(401, "로그인에 실패하였습니다.");
+    @ExceptionHandler(LoginFailureException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public Response loginFailureException() {
+        return Response.failure(401, "로그인에 실패하였습니다.");
+    }
 
-    // 409 응답
-    // nickname 중복
-//    @ExceptionHandler(MemberNicknameAlreadyExistsException.class)
-//    @ResponseStatus(HttpStatus.CONFLICT)
-//    public Response memberNicknameAlreadyExistsException(MemberNicknameAlreadyExistsException e) {
-//        return Response.failure(409, e.getMessage() + "은 중복된 닉네임 입니다.");
-
-    // 404 응답
-    // 요청한 자원을 찾을 수 없음
+        // 404 응답
+        // 요청한 자원을 찾을 수 없음
 //    @ExceptionHandler(MemberNotFoundException.class)
 //    @ResponseStatus(HttpStatus.NOT_FOUND)
 //    public Response memberNotFoundException() {
 //        return Response.failure(404, "요청한 회원을 찾을 수 없습니다.");
+//    }
 
-    // 404 응답
-    // 요청한 자원을 찾을 수 없음
-    @ExceptionHandler(RoleNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Response roleNotFoundException() {
-        return Response.failure(404, "요청한 권한 등급을 찾을 수 없습니다.");
+        // 404 응답
+        // 요청한 자원을 찾을 수 없음
+//    @ExceptionHandler(RoleNotFoundException.class)
+//    @ResponseStatus(HttpStatus.NOT_FOUND)
+//    public Response roleNotFoundException () {
+//        return Response.failure(404, "요청한 권한 등급을 찾을 수 없습니다.");
+//    }
+
+    // 409 응답
+    // username 중복
+    @ExceptionHandler(MemberUsernameAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Response memberUsernameAlreadyExistException (MemberNicknameAlreadyExistsException e){
+        return Response.failure(409, e.getMessage() + "은 중복된 아이디 입니다.");
     }
 
+    // 409 응답
+    // nickname 중복
+    @ExceptionHandler(MemberNicknameAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Response memberNicknameAlreadyExistsException (MemberNicknameAlreadyExistsException e){
+        return Response.failure(409, e.getMessage() + "은 중복된 닉네임 입니다.");
+    }
 }
---
+++ src/main/java/comu/community/exception/CannotConvertHelperException.java
@@ -0,0 +1,7 @@
+package comu.community.exception;
+
+public class CannotConvertHelperException extends RuntimeException{
+    public CannotConvertHelperException(String message) {
+        super(message);
+    }
+}
