e325e404668c70b009047b30177f355ce1446b51
seunghwanLee <dkqlch@naver.com>
2022-11-25 18:39:21 +0900

    Message 기능을 위한 config

--- src/main/java/comu/community/config/SecurityConfig.java
+++ src/main/java/comu/community/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import comu.community.config.jwt.JwtSecurityConfig;
 import comu.community.config.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static comu.community.entity.user.Role.ROLE_USER;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -44,7 +47,6 @@ public class SecurityConfig {
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .accessDeniedHandler(jwtAccessDeniedHandler)
 
-
                 // 시큐리티는 기본적으로 세션을 사용
                 // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
                 .and()
@@ -56,14 +58,25 @@ public class SecurityConfig {
                 .authorizeRequests()
 
                 .antMatchers("/auth/**").permitAll() //누구나 접근 가능
-
+                .antMatchers("/swagger-ui/**", "/v3/**").permitAll() // swagger
                 .antMatchers("/test")
                 .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
 
-                .antMatchers("/all").permitAll() // 누구나 접근 가능
+                .antMatchers("/api/sign-up", "/api/sign-in", "/api/reissue").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/users").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.GET, "/api/users/{id}").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.PUT, "/api/users/{id}").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.DELETE, "/api/users/{id}").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
 
-                .anyRequest().authenticated() // 그 외에는 인증 필요
-//                .anyRequest().permitAll() // 누구나 접근 가능
+                .antMatchers(HttpMethod.POST, "/api/messages").authenticated()
+                .antMatchers(HttpMethod.GET,"/api/messages/sender").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.GET,"/api/messages/sender/{id}").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.GET,"/api/messages/receiver").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.GET,"/api/messages/receiver/{id}").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.DELETE,"/api/messages/sender/{id}").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.DELETE,"/api/messages/receiver/{id}").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+
+                .anyRequest().hasAnyRole("ROLE_ADMIN")
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
                 .and()
