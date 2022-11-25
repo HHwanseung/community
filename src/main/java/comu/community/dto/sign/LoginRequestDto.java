cb5210414b3054b4c0d000a12d063d54f3534b09
seunghwanLee <dkqlch@naver.com>
2022-11-25 18:38:51 +0900

    User 로그인

---
+++ src/main/java/comu/community/dto/sign/LoginRequestDto.java
@@ -0,0 +1,41 @@
+package comu.community.dto.sign;
+
+import comu.community.entity.user.Role;
+import comu.community.entity.user.User;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiOperation(value = "로그인 요청")
+public class LoginRequestDto {
+
+    @ApiModelProperty(value = "아이디", notes = "아이디를 입력해주세요", required = true)
+    @NotBlank(message = "{LoginRequestDto.username.notBlank}")
+    private String username;
+
+    @ApiModelProperty(value = "비밀번호", required = true)
+    @NotBlank(message = "{LoginRequestDto.password.notBlank}")
+    private String password;
+
+    public User toMember(PasswordEncoder passwordEncoder) {
+        return User.builder()
+                .username(username)
+                .password(passwordEncoder.encode(password))
+                .role(Role.ROLE_USER)
+                .build();
+    }
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(username,password);
+    }
+
+}
