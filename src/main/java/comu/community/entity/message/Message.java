9695f6a6221af90d2ed9830a92977bc89f979536
seunghwanLee <dkqlch@naver.com>
2022-11-27 20:26:22 +0900

    Message entity

---
+++ src/main/java/comu/community/entity/message/Message.java
@@ -0,0 +1,65 @@
+package comu.community.entity.message;
+
+import comu.community.entity.user.User;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private boolean deletedBySender;
+
+    @Column(nullable = false)
+    private boolean deletedByReceiver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    private User receiver;
+
+    public Message(String title, String content, User sender, User receiver) {
+        this.title = title;
+        this.content = content;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public void deleteBySender() {
+        this.deletedBySender = true;
+    }
+
+    public void deleteByReceiver() {
+        this.deletedByReceiver = true;
+    }
+
+    public boolean isDeletedMessage() {
+        return isDeletedByReceiver() && isDeletedBySender();
+        // 둘다 삭제되면 true 리턴
+    }
+
+
+}
