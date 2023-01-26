3303f88f96204bf07f7d32000f6414d41a9c2b67
seunghwanLee <dkqlch@naver.com>
2023-01-26 21:29:49 +0900

    @PropertySource로 참고경로 설정

--- src/main/java/comu/community/config/redis/RedisConfig.java
+++ src/main/java/comu/community/config/redis/RedisConfig.java
@@ -3,6 +3,7 @@ package comu.community.config.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
+@PropertySource("classpath:")
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
