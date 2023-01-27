b53f68c414647b2d4ce9dc3d736f7a21a39fd0cc
seunghwanLee <dkqlch@naver.com>
2023-01-27 22:10:51 +0900

    AuthServiceImpl SignIn Redis를 이용하여 Redis 저장소에 저장

--- src/main/java/comu/community/service/auth/AuthServiceImpl.java
+++ src/main/java/comu/community/service/auth/AuthServiceImpl.java
@@ -10,10 +10,12 @@ import comu.community.exception.MemberNicknameAlreadyExistsException;
 import comu.community.exception.UsernameAlreadyExistsException;
 import comu.community.repository.refreshToken.RefreshTokenRepository;
 import comu.community.repository.member.MemberRepository;
+import comu.community.service.redis.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +30,7 @@ public class AuthServiceImpl implements AuthService {
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final RedisService redisService;
 
     @Override
     public void signup(SignUpRequestDto req) {
@@ -37,32 +40,26 @@ public class AuthServiceImpl implements AuthService {
 
     }
 
-    @Override
+
+
+        @Override
     public TokenResponseDto signIn(LoginRequestDto req) {
         Member member = memberRepository.findByUsername(req.getUsername()).orElseThrow(() -> {
-            throw new LoginFailureException();
+            return new LoginFailureException();
         });
 
         validatePassword(req, member);
-        Authentication authentication = getUserAuthentication(req);
-        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
-        RefreshToken refreshToken = buildRefreshToken(authentication, tokenDto);
-        refreshTokenRepository.save(refreshToken);
-        return new TokenResponseDto(tokenDto.getAccessToken(), tokenDto.getRefreshToken());
-    }
-
-    private RefreshToken buildRefreshToken(Authentication authentication, TokenDto tokenDto) {
-        return RefreshToken.builder()
-                .key(authentication.getName())
-                .value(tokenDto.getRefreshToken())
-                .build();
-    }
-
-    private Authentication getUserAuthentication(LoginRequestDto req) {
         UsernamePasswordAuthenticationToken authenticationToken = req.toAuthentication();
+
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        return authentication;
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+        redisService.setValues(authentication.getName(), tokenDto.getRefreshToken());
+        TokenResponseDto tokenResponseDto = new TokenResponseDto(tokenDto.getAccessToken(), tokenDto.getRefreshToken());
+        return tokenResponseDto;
     }
+
+
 
     @Override
     public TokenResponseDto reissue(TokenRequestDto tokenRequestDto) {
@@ -118,47 +115,4 @@ public class AuthServiceImpl implements AuthService {
             throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
         }
     }
-
-//    @Override
-//    public TokenResponseDto signIn(LoginRequestDto req) {
-//        User user = userRepository.findByUsername(req.getUsername()).orElseThrow(() -> {
-//            return new LoginFailureException();
-//        });
-//
-//        validatePassword(req, user);
-//        UsernamePasswordAuthenticationToken authenticationToken = req.toAuthentication();
-//
-//        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-//        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
-//
-//        redisService.setValues(authentication.getName(), tokenDto.getRefreshToken());
-//        TokenResponseDto tokenResponseDto = new TokenResponseDto(tokenDto.getAccessToken(), tokenDto.getRefreshToken());
-//        return tokenResponseDto;
-//    }
-//
-//
-//    @Override
-//    public TokenResponseDto reissue(TokenRequestDto tokenRequestDto) {
-//        Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
-//        redisService.checkRefreshToken(authentication.getName(), tokenRequestDto.getRefreshToken());
-//
-//        // 예외 처리 통과후 토큰 재생성
-//        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
-//        TokenResponseDto tokenResponseDto = new TokenResponseDto(tokenDto.getAccessToken(), tokenDto.getRefreshToken());
-//        return tokenResponseDto;
-//    }
-//
-//
-//    private void validateSignUpInfo(SignUpRequestDto signUpRequestDto) {
-//        if (userRepository.existsByUsername(signUpRequestDto.getUsername()))
-//            throw new MemberUsernameAlreadyExistsException(signUpRequestDto.getUsername());
-//        if (userRepository.existsByNickname(signUpRequestDto.getNickname()))
-//            throw new MemberNicknameAlreadyExistsException(signUpRequestDto.getNickname());
-//    }
-//
-//    private void validatePassword(LoginRequestDto loginRequestDto, User user) {
-//        if (!passwordEncoder.matches(loginRequestDto.getPassword(), user.getPassword())) {
-//            throw new LoginFailureException();
-//        }
-//    }
 }
