a76e0697dcce8f791b2bfcccf85a180cef9d3627
seunghwanLee <dkqlch@naver.com>
2023-02-19 22:08:33 +0900

    Point API Redis를 이용한 랭킹 시스템

--- src/main/java/comu/community/service/point/PointService.java
+++ src/main/java/comu/community/service/point/PointService.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface PointService {
 
-    PointRankingRedisResponseDto findPointRankKingWithMysql(Integer points);
+    PointRankingWithPagingResponseDto findPointRankKingWithMysql(Integer page);
     List<PointRankingRedisResponseDto> findPointsRankingWithRedis();
     void updatePoint(String username);
 
--- src/main/java/comu/community/service/point/PointServiceImpl.java
+++ src/main/java/comu/community/service/point/PointServiceImpl.java
@@ -1,7 +1,55 @@
 package comu.community.service.point;
 
-public class PointServiceImpl {
+import comu.community.config.constant.Constant;
+import comu.community.dto.point.PointRankingRedisResponseDto;
+import comu.community.dto.point.PointRankingSimpleDto;
+import comu.community.dto.point.PointRankingWithPagingResponseDto;
+import comu.community.dto.point.RankingPageInfoDto;
+import comu.community.entity.point.Point;
+import comu.community.repository.Point.PointRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class PointServiceImpl implements PointService {
 
+    private final static String RAKING_KEY = Constant.REDIS_RANKING_KEY;
+    private final RedisTemplate redisTemplate;
+    private final PointRepository pointRepository;
+
+    @Override
+    public PointRankingWithPagingResponseDto findPointRankKingWithMysql(Integer page) {
+        Page<Point> points = makePointRankingPages(page);
+        return responseRankPaging(points);
+    }
+
+    private PointRankingWithPagingResponseDto responseRankPaging(Page<Point> points) {
+        List<PointRankingSimpleDto> pointRanking = points.stream().map(point -> new PointRankingSimpleDto().toDto(point)).collect(Collectors.toList());
+        return PointRankingWithPagingResponseDto.toDto(pointRanking, new RankingPageInfoDto(points));
+    }
+
+    private Page<Point> makePointRankingPages(Integer page) {
+        PageRequest pageRequest = PageRequest.of(page, 10, Sort.by("point").descending());
+        return pointRepository.findAll(pageRequest);
+    }
+
+    @Override
+    public List<PointRankingRedisResponseDto> findPointsRankingWithRedis() {
+        return null;
+    }
+
+    @Override
+    public void updatePoint(String username) {
+
+    }
 }
