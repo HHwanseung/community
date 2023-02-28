592663c71cb5351c4fdee8e83aed38cabd63a524
seunghwanLee <dkqlch@naver.com>
2023-02-28 19:32:47 +0900

    주석정리

--- src/main/java/comu/community/response/Response.java
+++ src/main/java/comu/community/response/Response.java
@@ -5,7 +5,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)  //  null 값을 가지는 필드는, JSON 응답에 미포함
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class Response {
@@ -14,15 +14,15 @@ public class Response {
     private int code;
     private Result result;
 
-    public static Response success() { // 4
+    public static Response success() {
         return new Response(true, 0, null);
     }
 
-    public static <T> Response success(T data) { // 5
+    public static <T> Response success(T data) {
         return new Response(true, 0, new Success<>(data));
     }
 
-    public static Response failure(int code, String msg) { // 6
+    public static Response failure(int code, String msg) {
         return new Response(false, code, new Failure(msg));
     }
 
