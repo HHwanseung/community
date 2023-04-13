972f9816554d86f13b65e20965630ef45bec6e90
seunghwanLee <dkqlch@naver.com>
2023-04-13 15:47:14 +0900

    CommentDTO 네이밍 리팩토링

--- src/main/java/comu/community/dto/comment/CommentDto.java
+++ src/main/java/comu/community/dto/comment/CommentDto.java
@@ -1,7 +1,7 @@
 package comu.community.dto.comment;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import comu.community.dto.member.MemberEditRequestDto;
+import comu.community.dto.member.MemberSimpleNicknameResponseDto;
 import comu.community.entity.comment.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,13 +15,13 @@ public class CommentDto {
 
     private Long id;
     private String content;
-    private MemberEditRequestDto memberEditRequestDto;
+    private MemberSimpleNicknameResponseDto memberSimpleNicknameResponseDto;
 
     public CommentDto toDto(Comment comment) {
         return new CommentDto(
                 comment.getId(),
                 comment.getContent(),
-                MemberEditRequestDto.toDto(comment.getMember())
+                MemberSimpleNicknameResponseDto.toDto(comment.getMember())
         );
     }
 }
