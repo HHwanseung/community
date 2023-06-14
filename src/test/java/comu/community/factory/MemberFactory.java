953b5ebcb7508736eab87f83eaa60bd2285d0c6e
seunghwanLee <dkqlch@naver.com>
2023-06-14 18:47:40 +0900

    Junit5 test MemberInfo

--- src/test/java/comu/community/factory/MemberFactory.java
+++ src/test/java/comu/community/factory/MemberFactory.java
@@ -3,19 +3,22 @@ package comu.community.factory;
 import comu.community.entity.member.Role;
 import comu.community.entity.member.Member;
 
-public class UserFactory {
+public class MemberFactory {
 
     public static Member createUserWithAdminRole() {
-        Member member = new Member("username", "password", Role.ROLE_ADMIN);
+        Member member = new Member("username", "password", Role.ROLE_USER);
         member.setName("name1");
         member.setNickname("nickname1");
         return member;
     }
 
-    public static Member createUser() {
-        Member member = new Member("username", "password", Role.ROLE_ADMIN);
-        member.setName("name1");
-        member.setNickname("nickname1");
+    public static Member createMember() {
+        Member member = Member.builder()
+                .username("username1")
+                .name("name1")
+                .password("1234")
+                .role(Role.ROLE_USER)
+                .build();
         return member;
     }
 
@@ -32,5 +35,13 @@ public class UserFactory {
         member.setNickname("nickname2");
         return member;
     }
+
+    public static Member createAdmin() {
+        Member member = new Member("admin1","admin1",Role.ROLE_ADMIN);
+        member.setName("admin1");
+        member.setNickname("admin1");
+        return member;
+    }
+
 
 }
